e4060ce23120d1d3e2076c10a6ddeb2c9a1387fa
unknown <abdul.j@medlinkjobs.com>
2023-05-27 12:30:12 +0600

    first commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use("/product", productRoutes);
 app.use("/user", userRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello Food Mart Server is connected!')
+  res.send('Hello Food Mart Server is connected 1!')
 })
 
 app.listen(port, () => {
